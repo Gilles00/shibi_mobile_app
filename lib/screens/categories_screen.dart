26e9ce122ab3772941fc3e1b059fc482fb0ccaa7
Damilola Adekoya <damilolaadekoya@Damilolas-MacBook-Pro.local>
2020-02-23 22:17:42 +0100

    feature(list-categories):listing all categories on the app

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import '../dummy_data.dart';
+import '../category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Shibi'),
+      ),
+      body: GridView(
+        padding: EdgeInsets.all(10),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+        children: DUMMY_CATEGORIES
+            .map((category) => CategoryItem(category.title, category.color))
+            .toList(),
+      ),
+    );
+  }
+}
