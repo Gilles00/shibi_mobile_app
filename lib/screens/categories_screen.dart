18cc508d22c232df6770a4f9b6c7b699a487b8e0
Damilola Adekoya <damilolaadekoya@Damilolas-MacBook-Pro.local>
2020-02-28 00:33:49 +0100

    feature(top-bar):create a nav bar

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -5,24 +5,19 @@ import '../widgets/category_item.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Shibi'),
-      ),
-      body: GridView(
-        padding: EdgeInsets.all(10),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 200,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 20,
-            mainAxisSpacing: 20),
-        children: DUMMY_CATEGORIES
-            .map(
-              (category) =>
-                  CategoryItem(category.id, category.title, category.color),
-            )
-            .toList(),
-      ),
+    return GridView(
+      padding: EdgeInsets.all(10),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20),
+      children: DUMMY_CATEGORIES
+          .map(
+            (category) =>
+                CategoryItem(category.id, category.title, category.color),
+          )
+          .toList(),
     );
   }
 }
